3a6284013231524c561cf6d26145942b492fb00c
Martin Kurylowski <mkurylowski@gmail.com>
2024-10-06 19:22:16 -0400

    protect routes2

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,4 +1,4 @@
-import { useState} from 'react'
+import { useState, Suspense } from 'react'
 import { Link, useSearchParams, useLoaderData, defer, Await } from 'react-router-dom'
 import { getVans } from '../../api'
 
@@ -29,68 +29,72 @@ export default function Vans() {
             }} aria-live="assertive">There was an error: {error.message}</h1>
     }
 
+    function renderVanElements(vans) {
+            const displayedVans = typeFilter ? 
+                vans.filter(van => van.type.toLowerCase() === typeFilter) 
+                : vans
+
+            const vansElements = displayedVans.map((van, index) => (
+                    <div key={van.id} className="van">
+                        <Link 
+                            to={`/vans/${van.id}`} 
+                            state={{ 
+                                search: `?${searchParams.toString()}`, 
+                                type: typeFilter
+                            }}
+                            aria-label={`View details for ${van.name}, priced at ${van.price} per day`}
+                        >
+                        <img className="van-image" src={van.imageUrl}/><br/>
+                        <div className="van-details-wrap">
+                            <div className="van-name-btn">
+                                {van.name}
+                                <button className={van.type + "-vans"}>{van.type}</button>
+                            </div>
+                            <div className="van-price">
+                                ${van.price}<br/>
+                                <h5 className="timeframe">/day</h5>
+                            </div>
+                        </div>
+                        </Link>            
+                    </div>
+            ))
+            return (
+                <>
+                <div className="vans-filter-options">
+                    <button onClick={() => 
+                        handleFilterChange("type", "simple")} 
+                        className={typeFilter === "simple" ? "simple-vans-active" : "simple-vans"}>Simple
+                    </button>
+                    <button onClick={() => 
+                        handleFilterChange("type", "luxury")} 
+                        className={typeFilter === "luxury" ? "luxury-vans-active" : "luxury-vans"}>Luxury
+                        </button>
+                    <button onClick={() => 
+                        handleFilterChange("type","rugged")} 
+                        className={typeFilter === "rugged" ? "rugged-vans-active" : "rugged-vans"}>Rugged
+                    </button>
+                    {typeFilter && <button onClick={() => 
+                        handleFilterChange("type", null)} 
+                        className="clear-filter">Clear filters
+                    </button>}
+                </div>
+
+                <div className="vans-list">
+                {vansElements}              
+                </div>
+                </>
+            )
+    }
+
     return (
         <div className="vans-container">
             <div className="vans-filters">
                 <h1>Explore our van options</h1>
-                <Await resolve={dataPromise.vans}>
-                    {vans => {
-                        const displayedVans = typeFilter ? 
-                            vans.filter(van => van.type.toLowerCase() === typeFilter) 
-                            : vans
-
-                        const vansElements = displayedVans.map((van, index) => (
-                                <div key={van.id} className="van">
-                                    <Link 
-                                        to={`/vans/${van.id}`} 
-                                        state={{ 
-                                            search: `?${searchParams.toString()}`, 
-                                            type: typeFilter
-                                        }}
-                                        aria-label={`View details for ${van.name}, priced at ${van.price} per day`}
-                                    >
-                                    <img className="van-image" src={van.imageUrl}/><br/>
-                                    <div className="van-details-wrap">
-                                        <div className="van-name-btn">
-                                            {van.name}
-                                            <button className={van.type + "-vans"}>{van.type}</button>
-                                        </div>
-                                        <div className="van-price">
-                                            ${van.price}<br/>
-                                            <h5 className="timeframe">/day</h5>
-                                        </div>
-                                    </div>
-                                    </Link>            
-                                </div>
-                        ))
-                        return (
-                            <>
-                            <div className="vans-filter-options">
-                                <button onClick={() => 
-                                    handleFilterChange("type", "simple")} 
-                                    className={typeFilter === "simple" ? "simple-vans-active" : "simple-vans"}>Simple
-                                </button>
-                                <button onClick={() => 
-                                    handleFilterChange("type", "luxury")} 
-                                    className={typeFilter === "luxury" ? "luxury-vans-active" : "luxury-vans"}>Luxury
-                                    </button>
-                                <button onClick={() => 
-                                    handleFilterChange("type","rugged")} 
-                                    className={typeFilter === "rugged" ? "rugged-vans-active" : "rugged-vans"}>Rugged
-                                </button>
-                                {typeFilter && <button onClick={() => 
-                                    handleFilterChange("type", null)} 
-                                    className="clear-filter">Clear filters
-                                </button>}
-                            </div>
-
-                            <div className="vans-list">
-                            {vansElements}              
-                            </div>
-                            </>
-                        )
-                    }}
-                </Await>
+                <Suspense fallback={<h2>Loading Vans...</h2>}>
+                    <Await resolve={dataPromise.vans}>
+                        {renderVanElements}
+                    </Await>
+                </Suspense>
             </div>
         </div>
     )
